f60415b3d69c6e3dcfa1aa1e4012878ecd513128
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2009-10-19 22:38:09 -0400

    git taking some time to get used to

--- src/bifid/bifid.clj
+++ src/bifid/bifid.clj
@@ -1,15 +1,68 @@
+(comment
+ This is my effort to implement Bifid cipher using Clojure.
+)
 
-(ns org.bifid
-  (:genclass)
-  ;(:require )
-  ;(:use )
-  ;(:import )
-  )
+(ns bifid
+  (:gen-class))
 
 
 (defn polybius-square
-  "Generate a map corresponding to the polybius square."
+  "Polybius square by the given charset and size"
+  [charset square-size]
+  (map #(vector %1 [%2 %3]) charset
+    (for [x (range 1 (+ 1 square-size)), y (range 1 (+ 1 square-size))] x)
+    (take (count charset) (cycle (range 1 (+ 1 square-size))))))
+
+(defn- letter-2-code
+  "Produce a mapping between letters and their codes"
+  [square]
+  (letfn [(helper [the-square the-map]
+          (if (empty? the-square)
+            the-map
+            (let [the-item (first the-square)
+                  the-key (first the-item)
+                  the-number (second the-item)]
+              (recur (rest the-square) (assoc the-map the-key the-number)))))]
+    (helper square (empty hash-map))))
+
+(defn- transposed-2-letter
+  "Produce a mapping between the transposed codes to letter"
+  [square]
+  (letfn [(helper [the-square the-map]
+            (if (empty? the-square)
+              the-map
+              (let [the-item (first the-square)
+                    the-key (second the-item)
+                    the-letter (first the-item)]
+                (recur (rest the-square) (assoc the-map the-key the-letter)))))]
+    (helper square (empty hash-map))))
+
+(defn encode
+  "Return the message encoded by the specified polybius-square"
+  [message polybius-square]
+  (let [l2c (letter-2-code polybius-square)
+        tr2l (transposed-2-letter polybius-square)]
+    (apply str
+      (map #(get tr2l % %) (partition 2 (apply interleave (map #(get l2c % %) message)))))))
+
+(defn decode
+  "Return the decoded message using the specified polybius-square"
+  [encoded polybius-square]
+  (let [l2c (letter-2-code polybius-square)
+        c2l (transposed-2-letter polybius-square)
+        codes (apply concat (map #(get l2c % %) encoded))
+        len (count codes)
+        parts (partition (/ len 2) codes)]
+    (apply str (map #(get c2l %1 %1) (map #(vector %1 %2) (first parts) (second parts))))))
+
+(defn -main
+  "A simple test case entry point"
   []
-  (map #(vector %1 %2 %3) "ABCDEFGHIKLMNOPQRSTUVWXYZ" 
-    (reduce #(concat %1 %2) (repeat 5 [1 2 3 4 5]))
-    (for [x (range 1 6) y (range 1 6)] x)))
+  (let [square (polybius-square "ABCDEFGHIKLMNOPQRSTUVWXYZ" 5)]
+    (do
+        (println (encode "PROGRAMMINGPRAXIS" square))
+        (println (decode "OMQNHHQWUIGBIMWCS" square)))))
+
+
+
+
